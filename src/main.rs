d5c7ef64a1bd9be90dd533a7992ccea4df92f81d
pigpiglala <1060279778@qq.com>
2025-08-26 22:12:23 +0800

    draw the calc interface

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,79 @@
 #![windows_subsystem = "windows"]
 
-use eframe::egui::{self, Color32, Painter, Pos2, Rect, Vec2};
+use eframe::egui::{self, Color32, Painter, Pos2, Rect, Vec2, StrokeKind};
 
-fn show_block(ui: &mut egui::Ui) {
+fn result_block(ui: &mut egui::Ui) {
     let full_rect = ui.max_rect();
     let block_size = Vec2::new(full_rect.width(), full_rect.height() / 5.0);
-
     let block_rect = Rect::from_min_size(full_rect.min, block_size);
-
     let painter: &Painter = ui.painter();
     painter.rect_filled(
         block_rect, 
         5.0,  
-        Color32::from_rgba_premultiplied(150, 150, 150, 60),
+        Color32::from_rgb(20, 26, 34), // #141A22
     );
 }
+
+fn input_block(ui: &mut egui::Ui) {
+    let full_rect = ui.max_rect();
+
+    let result_h = full_rect.height() / 5.0;
+    let top_left = Pos2::new(full_rect.min.x, full_rect.min.y + result_h);
+    let block_size = Vec2::new(full_rect.width(), full_rect.height() - result_h);
+    let block_rect = Rect::from_min_size(top_left, block_size);
+    let painter: &Painter = ui.painter();
+    painter.rect_filled(
+        block_rect, 
+        5.0,  
+        Color32::from_rgb(42, 48, 58), // #2A303A
+    );
+    let cell_w = block_rect.width() / 4.0;
+    let cell_h = block_rect.height() / 5.0;
+    
+    let gap = 4.0; 
+
+    let labels = [
+        ["CE", "C", "DEL", "/"],
+        ["7", "8", "9", "*"],
+        ["4", "5", "6", "-"],
+        ["1", "2", "3", "+"],
+        ["%", "0", ".", "="],
+    ];
+
+    for (row_i, row) in labels.iter().enumerate() {
+        for (col_i, &text) in row.iter().enumerate() {
+            let x = block_rect.min.x + col_i as f32 * cell_w;
+            let y = block_rect.min.y + row_i as f32 * cell_h;
+            
+            let button_rect = Rect::from_min_size(
+                Pos2::new(x + gap / 2.0, y + gap / 2.0), 
+                Vec2::new(cell_w - gap, cell_h - gap)
+            );
+
+            if ui
+                .interact(button_rect, ui.id().with((row_i, col_i)), egui::Sense::click())
+                .clicked()
+            {
+                println!("Pressed {text}");
+            }
+
+            painter.rect_stroke(
+                button_rect,
+                egui::Rounding::same(8),
+                egui::Stroke::new(0.4, Color32::GRAY),
+                egui::StrokeKind::Inside,
+            );
+            painter.text(
+                button_rect.center(),
+                egui::Align2::CENTER_CENTER,
+                text,
+                egui::FontId::proportional(20.0),
+                Color32::WHITE,
+            );
+        }
+    }
+}
+
 
 fn main() -> Result<(), eframe::Error> {
     // create a window
@@ -53,7 +112,8 @@ impl TinyCalc {
 impl eframe::App for TinyCalc {
     fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
         egui::CentralPanel::default().show(ctx, |ui| {
-            show_block(ui);
+            result_block(ui);
+            input_block(ui);
         });
     }
 }
